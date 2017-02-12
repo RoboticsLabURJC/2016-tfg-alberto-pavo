e1d1386df6b2b28522ae244d23cbdb56c9c3d72e
Alberto Pavo Blas <Alberto@MacBook-Pro-de-Alberto.local>
2017-02-12 16:58:57 +0100

    final version

--- ffmpegAdapter/ffmpeg-adapter.py
+++ ffmpegAdapter/ffmpeg-adapter.py
@@ -1,24 +1,3 @@
-#!/usr/bin/python3.5
-#
-#  Copyright (C) 1997-2016 JDE Developers Team
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see http://www.gnu.org/licenses/.
-#  Authors :
-#       Alberto Martin Florido <almartinflorido@gmail.com>
-#       Aitor Martinez Fernandez <aitor.martinez.fernandez@gmail.com>
-#
-
 import sys
 import easyiceconfig as EasyIce
 from gui.threadGUI import ThreadGUI
@@ -34,18 +13,16 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 if __name__ == '__main__':
     ic = EasyIce.initialize(sys.argv)
     prop = ic.getProperties()
-    remoteCamera = CameraClient(ic, "Introrob.Camera", True)
+    camera = CameraClient(ic, "Introrob.Camera", True)
     app = QApplication(sys.argv)
-    camera = CameraWidget()
-    camera.setCamera(remoteCamera)
-    if (len(sys.argv)== 3 and sys.argv[2] == "GUI"):
-        camera.setGUI = True
-        camera.initUI()
-        camera.show()
+    frame = CameraWidget()
+    frame.setCamera(camera)
+    if len(sys.argv == 3 and sys.argv[2] == "GUI"):
+        frame.show()
     else:
         print("For see the GUI, add to command GUI")
 
-    t2 = ThreadGUI(camera)  
+    t2 = ThreadGUI(frame)  
     t2.daemon=True
     t2.start()
     
--- ffmpegAdapter/gui/cameraWidget.py
+++ ffmpegAdapter/gui/cameraWidget.py
@@ -1,5 +1,7 @@
+from PyQt5.QtCore import QSize, pyqtSignal
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QPushButton,QWidget, QLabel
+from gui.communicator import Communicator
 from PIL import Image
 import os
 
@@ -20,11 +22,10 @@ class CameraWidget(QWidget):
         super(CameraWidget, self).__init__(parent)
         self.imageUpdate.connect(self.updateImage)
         self.updGUI.connect(self.updateGUI)
-        self.GUI = False;
-
+        self.initUI()
+        
     def initUI(self):
-        self.GUI = True;
-
+        
         self.setMinimumSize(680,500)
         self.setMaximumSize(680,500)
         
@@ -51,23 +52,19 @@ class CameraWidget(QWidget):
             im = Image.fromarray(img)
             im.save("temp.jpg")
             os.rename("temp.jpg","imagen.jpg")
-            if self.GUI:
-                showGUI(img)
-
-    def showGUI(self,img):
-        image = QImage(img.data, img.shape[1], img.shape[0], img.shape[1]*img.shape[2], QImage.Format_RGB888);
-
-        if img.shape[1]==self.IMAGE_COLS_MAX:
-            x=20
-        else:
-            x=(self.IMAGE_COLS_MAX+20)/2-(img.shape[1]/2)
-        if img.shape[0]==self.IMAGE_ROWS_MAX:
-            y=40
-        else:
-            y=(self.IMAGE_ROWS_MAX+40)/2-(img.shape[0]/2)
-
-        size=QSize(img.shape[1],img.shape[0])
-        self.imgLabel.move(x,y)
-        self.imgLabel.resize(size)
-        self.imgLabel.setPixmap(QPixmap.fromImage(image))
-
+            image = QImage(img.data, img.shape[1], img.shape[0], img.shape[1]*img.shape[2], QImage.Format_RGB888);
+        
+            if img.shape[1]==self.IMAGE_COLS_MAX:
+                x=20
+            else:
+                x=(self.IMAGE_COLS_MAX+20)/2-(img.shape[1]/2)
+            if img.shape[0]==self.IMAGE_ROWS_MAX:
+                y=40
+            else:
+                y=(self.IMAGE_ROWS_MAX+40)/2-(img.shape[0]/2)
+            
+            size=QSize(img.shape[1],img.shape[0])
+            self.imgLabel.move(x,y)
+            self.imgLabel.resize(size)
+            self.imgLabel.setPixmap(QPixmap.fromImage(image))
+        
