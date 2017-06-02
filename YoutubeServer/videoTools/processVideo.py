2767515df63997fcab47776e02160a3f0e0a7985
Alberto Pavo Blas <Alberto@MacBook-Pro-de-Alberto.local>
2017-06-02 18:25:27 +0200

    YouTube server first version

---
+++ YoutubeServer/videoTools/processVideo.py
@@ -0,0 +1,41 @@
+import os
+import shlex,subprocess
+from subprocess import PIPE,Popen
+from datetime import datetime
+class processVideo():
+
+	def setURL(self,URL):
+		self.url = URL
+
+	def setFileList(self):
+		command = shlex.split('youtube-dl -f 92 -g ' + self.url)
+		process= Popen(command ,stdout=PIPE,stderr=PIPE)
+		self.fileList=process.stdout.read()
+		process.stdout.close()
+
+	def getImage(self,init_time,end_time):
+		init_time = datetime.strftime(init_time,'%H:%M:%S')
+		end_time = datetime.strftime(end_time,'%H:%M:%S')
+		command =shlex.split("ffmpeg -i output.ts -start_number 0 -vf fps=5 -ss " + init_time + " -to " + end_time + " -f image2 -updatefirst 1 temp.jpg")
+		process= Popen(command,stdout=PIPE,stderr=PIPE)
+	
+	def downloadVideo(self):
+		data = self.fileList.splitlines()
+		data= data[0].decode('utf-8')
+		command=shlex.split('ffmpeg -i ' + data + ' -c copy output.ts')
+		process= Popen(command,stdout=PIPE,stderr=PIPE)
+
+	def changeName(self):
+		if os.path.isfile('./temp.jpg'):
+			os.rename("temp.jpg","image.jpg")
+
+	def getVideoDuration(self):
+		command = shlex.split('ffprobe -show_entries format=duration -sexagesimal output.ts') 
+		process = Popen(command ,stdout=PIPE,stderr=PIPE)
+		time = process.stdout.read()
+		time = time.decode('utf-8')
+		time = time.split('\n')[1].split('=')[1]
+		time = datetime.strptime(time.split('.')[0],'%H:%M:%S')
+		process.stdout.close()
+		return time
+	
---
+++ YoutubeServer/youtubeServer.py
@@ -0,0 +1,45 @@
+import Ice
+import sys
+import JdeRobot
+from PIL import Image
+from easyiceconfig import easyiceconfig as EasyIce
+from videoTools.processVideo import processVideo
+from videoTools.threadFlow import ThreadImage,ThreadDownload,ThreadChangeName
+from JdeRobot.ImageProviderI import ImageProviderI,WorkQueue
+
+
+
+if __name__== "__main__":
+
+	try:
+		ic = EasyIce.initialize(sys.argv)
+		prop = ic.getProperties()
+		endpoint = prop.getProperty('youtubeServer.Endpoints')
+		URL = prop.getProperty('URL')
+		print(endpoint)
+		workQueue = WorkQueue()
+		workQueue.setDaemon(True)
+		dataFlow = processVideo()
+		dataFlow.setURL(URL)
+		dataFlow.setFileList()
+
+		downloadThread = ThreadDownload(dataFlow)
+		imageThread = ThreadImage(dataFlow)
+		nameThread = ThreadChangeName(dataFlow)
+
+		downloadThread.start()
+		imageThread.start()
+		nameThread.start()
+		workQueue.start()
+
+		adapter = ic.createObjectAdapterWithEndpoints("youtubeServer",endpoint) #Properties
+		object = ImageProviderI(workQueue)
+		adapter.add(object, Ice.stringToIdentity("youtubeServer"))
+		adapter.activate()
+		workQueue.join()
+		print('esperar a cierre')
+		ic.waitForShutdown()
+	except KeyboardInterrupt:
+		sys.exit()        
+
+  
