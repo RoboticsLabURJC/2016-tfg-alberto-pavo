03bd78e14e532485aca77e5c4214a77a08e5fa00
apavo <a.pavo93@gmail.com>
2017-10-12 17:42:26 +0200

    Update threadFlow.py

--- YoutubeServer/videoTools/threadFlow.py
+++ YoutubeServer/videoTools/threadFlow.py
@@ -4,7 +4,7 @@ from datetime import timedelta,datetime
 
 
 class ThreadImage(threading.Thread):
-	
+	#thread that extract images of video
 	def __init__(self,dataFlow):
 		self.dataFlow = dataFlow
 		self.format_time = '%H:%M:%S'
@@ -19,7 +19,11 @@ class ThreadImage(threading.Thread):
 		while(True):
 			self.end_time = self.dataFlow.getVideoDuration()
 			self.dataFlow.getImage(self.init_time,self.end_time)
-			self.init_time = self.end_time
+			#if video download finish, restart the proccess of extract images since init of video
+			if self.init_time != self.end_time:
+				self.init_time = self.end_time
+			else
+				self.init_time = datetime.strptime('00:00:00',self.format_time)
 
 class ThreadDownload(threading.Thread):
 	
@@ -32,7 +36,7 @@ class ThreadDownload(threading.Thread):
 		self.dataFlow.downloadVideo()
 
 class ThreadChangeName(threading.Thread):
-	
+	#thread that manage doble buffer
 	def __init__(self,dataFlow):
 		self.dataFlow = dataFlow
 		threading.Thread.__init__(self)
